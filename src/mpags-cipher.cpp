25b853a34a9930f7bbc90c66976e991f39b44966
Vlad-Ioan Tir <vxt578@student.bham.ac.uk>
2025-11-21 12:05:27 +0000

    create CMakeLists.txt, source and header files

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -3,11 +3,19 @@
 #include <string>
 #include <vector>
 
+// Our project headers
+#include "TransformChar.hpp"
+#include "ProcessCommandLine.hpp"
+// function declarations
+std::string transformChar(const char in);
+bool processCommandLine(const std::vector<std::string>& args,
+                        bool& helpRequested, bool& versionRequested,
+                        std::string& inputFile, std::string& outputFile);
+
 int main(int argc, char* argv[])
 {
     // Convert the command-line arguments into a more easily usable form
     const std::vector<std::string> cmdLineArgs{argv, argv + argc};
-    const std::size_t nCmdLineArgs{cmdLineArgs.size()};
 
     // Options that might be set by the command-line arguments
     bool helpRequested{false};
@@ -15,46 +23,14 @@ int main(int argc, char* argv[])
     std::string inputFile{""};
     std::string outputFile{""};
 
-    // Process command line arguments - ignore zeroth element, as we know this
-    // to be the program name and don't need to worry about it
-    for (std::size_t i{1}; i < nCmdLineArgs; ++i) {
-        if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-            helpRequested = true;
-        } else if (cmdLineArgs[i] == "--version") {
-            versionRequested = true;
-        } else if (cmdLineArgs[i] == "-i") {
-            // Handle input file option
-            // Next element is filename unless "-i" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -i requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                inputFile = cmdLineArgs[i + 1];
-                ++i;
-            }
-        } else if (cmdLineArgs[i] == "-o") {
-            // Handle output file option
-            // Next element is filename unless "-o" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -o requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                outputFile = cmdLineArgs[i + 1];
-                ++i;
-            }
-        } else {
-            // Have an unknown flag to output error message and return non-zero
-            // exit status to indicate failure
-            std::cerr << "[error] unknown argument '" << cmdLineArgs[i]
-                      << "'\n";
-            return 1;
-        }
+    // Process command line arguments
+    const bool cmdLineStatus{processCommandLine(
+        cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile)};
+
+    // Any failure in the argument processing means we can't continue
+    // Use a non-zero return value to indicate failure
+    if (!cmdLineStatus) {
+        return 1;
     }
 
     // Handle help, if requested
@@ -97,47 +73,7 @@ int main(int argc, char* argv[])
 
     // loop over each character from user input
     while (std::cin >> inputChar) {
-        // Uppercase alphabetic characters
-        if (std::isalpha(inputChar)) {
-            inputText += std::toupper(inputChar);
-            continue;
-        }
-
-        // Transliterate digits to English words
-        switch (inputChar) {
-            case '0':
-                inputText += "ZERO";
-                break;
-            case '1':
-                inputText += "ONE";
-                break;
-            case '2':
-                inputText += "TWO";
-                break;
-            case '3':
-                inputText += "THREE";
-                break;
-            case '4':
-                inputText += "FOUR";
-                break;
-            case '5':
-                inputText += "FIVE";
-                break;
-            case '6':
-                inputText += "SIX";
-                break;
-            case '7':
-                inputText += "SEVEN";
-                break;
-            case '8':
-                inputText += "EIGHT";
-                break;
-            case '9':
-                inputText += "NINE";
-                break;
-        }
-
-        // If the character isn't alphabetic or numeric, DONT add it
+        inputText += transformChar(inputChar);
     }
 
     // Print out the transliterated text
@@ -154,3 +90,5 @@ int main(int argc, char* argv[])
     // and for consistency with other functions
     return 0;
 }
+
+
